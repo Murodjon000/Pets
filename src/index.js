4039038c5e3dc80afbd9b5e900af5afd57c0d6a8
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-05-03 22:02:22 +0500

    update inedx js file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import Router from "./Routes/PageRouter";
+import "./styles/App.scss";
+import reducer from "./reducers";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
 
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk)
+  // other store enhancers if any
+);
 
-const Main = ()=> (
+const store = createStore(reducer, enhancer);
+
+const Main = () => (
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router />
+    </Provider>
   </React.StrictMode>
-)
+);
 
-ReactDOM.render(<Main/>,  document.getElementById('root'));
-
+ReactDOM.render(<Main />, document.getElementById("root"));
