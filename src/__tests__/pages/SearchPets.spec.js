97237179b5316979158cb46bd01e1c4618680591
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-05-04 14:37:54 +0500

    add Searchpage page snapshot test

---
+++ src/__tests__/pages/SearchPets.spec.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import SearchPets from '../../Components/SearchPets'
+
+test('should correctly render SearchPets page', () => {
+  const component = renderer.create(<SearchPets />);
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
