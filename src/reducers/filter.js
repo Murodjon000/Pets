0d9e5d6e14364829a07e35b0005ff8db71d5a4b6
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-05-04 14:35:20 +0500

    update filter reducer

--- src/reducers/filter.js
+++ src/reducers/filter.js
@@ -2,15 +2,15 @@ import { FILTER_GENDER, FILTER_PETS, FILTER_SIZE } from "../actions";
 
 const filterPet = (state = "All", action) => {
   if (action.type === FILTER_PETS) {
-    return { ...state, breed: action.payload.breed };
+    return { state, breed: action.payload.breed };
   }
 
   if (action.type === FILTER_SIZE) {
-    return { ...state, size: action.payload.size };
+    return { state, size: action.payload.size };
   }
 
   if (action.type === FILTER_GENDER) {
-    return { ...state, gender: action.payload.gender };
+    return { state, gender: action.payload.gender };
   }
 
   return state;
