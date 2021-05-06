2b48a588fc492655668335252f1590a983050e85
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-05-06 14:29:05 +0500

    update add redux SearchPetsContainer helper

---
+++ src/containers/SearchPetsContainer.js
@@ -0,0 +1,24 @@
+import { connect } from 'react-redux';
+import {
+  fetchPets,
+  filterPets,
+  filterSize,
+  filterGender,
+} from '../actions/index';
+import SearchPets from '../Components/SearchPets';
+
+const mapStateToProps = (state) => ({
+  pets: state.pets,
+  filter: state.filter.breed,
+  size: state.filter.size,
+  gender: state.filter.gender,
+});
+
+const mapStateToDispatch = (dispatch) => ({
+  fetchPets: (animals) => dispatch(fetchPets(animals)),
+  filterPets: (breed) => dispatch(filterPets(breed)),
+  filterSize: (size) => dispatch(filterSize(size)),
+  filterGender: (gender) => dispatch(filterGender(gender)),
+});
+
+export default connect(mapStateToProps, mapStateToDispatch)(SearchPets);
